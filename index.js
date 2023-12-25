fd8b6f5ac871321f72d3b824afce49b942ae50d4
ESTHER WANJIKU <ESTHERWANJIQU@GMAIL.COM>
2023-12-25 19:16:22 +0300

    aster

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+//document.getElementsByTagName("main").removeAttribute;
+main.remove();
+let newHeader=document.createElement("h1");
+document.head.append(newHeader)[1];
+//h1.setAttribute("id","victory");
+//let h1=document.getElementById("victory")
+//victory.innerHTML="YOUR-NAME is the champion"
+let victory=document.createElement("id");
+newHeader.appendChild("victory");
+victory.textContent="YOUR-NAME is the champion";
